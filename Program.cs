82a48633793c04f7fd5066ccf14a0c32cbe33f0d
HerickSz <hericksouza586@gmail.com>
2022-08-11 21:13:48 -0300

    Iniciando o Repositorio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace teste1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string nome=Console.ReadLine();
+            Console.WriteLine("O nome digitado foi: "+nome);
+            Console.ReadLine();
+        }
+    }
+}
